5092d1acbdddb08f0ea2214274f16d4034ee7266
qwuadr1xx <qwuadrixx@gmail.com>
2025-06-01 11:04:09 +0500

    /bugfix дополнить обработчик ошибок /bugfix изменить pom'ики

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -2,12 +2,20 @@ package ru.practicum.shareit.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ErrorHandler {
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleThrowable(Throwable ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Произошла внутренняя ошибка сервера: " + ex.getMessage());
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
